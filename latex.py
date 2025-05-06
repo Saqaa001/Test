343f85cf7845d75fc69d1c704d0631d72f6ab2f5
Saqaa001 <sshax015@gmail.com>
2025-05-06 09:21:08 -0400

    Update latex.py

--- latex.py
+++ latex.py
@@ -3,11 +3,30 @@ import pandas as pd
 import re
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+from dotenv import load_dotenv
+import firebase_admin
+from firebase_admin import credentials
 
-# --- Firebase Init ---
+load_dotenv()  # Load .env
+
+firebase_config = {
+    "type": os.getenv("FIREBASE_TYPE"),
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+    "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_X509_CERT_URL")
+}
+
 if not firebase_admin._apps:
-    cred = credentials.Certificate("latex.json")
+    cred = credentials.Certificate(firebase_config)
     firebase_admin.initialize_app(cred)
+
 db = firestore.client()
 
 # --- Clear form early if flagged ---
